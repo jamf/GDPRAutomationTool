bb2a46a1763f8c5a17a5147f0b4af11e810d6359
Connor Temple <connor.temple@jamf.com>
2018-06-18 09:46:56 -0500

    RES-1267 Search multiple times

--- gdpr.py
+++ gdpr.py
@@ -4,41 +4,48 @@ import requests
 
 instance=input("Jamf Pro URL:")
 username=input("Jamf Pro Username:")
-password=getpass.getpass('Jamf Pro Password:')
-search_username=input("Search Username:")
+password=getpass.getpass("Jamf Pro Password:")
 
-api_url = 'https://'+instance+'/JSSResource'
+loop = True
+while loop:
+	search_username=input("Search Username:")
 
-# search jamf pro users
-jss_users_api=api_url+'/users/name/'+search_username
-jss_users_response = requests.get(jss_users_api, auth=(username, password), headers={'Accept': 'application/json'})
+	api_url = 'https://'+instance+'/JSSResource'
 
-if jss_users_response.status_code == 200:
-    jss_user = jss_users_response.json()
-    print(jss_user)
+	# search jamf pro users
+	jss_users_api=api_url+'/users/name/'+search_username
+	jss_users_response = requests.get(jss_users_api, auth=(username, password), headers={'Accept': 'application/json'})
 
-# search jamf pro user accounts
-jss_account_api=api_url+'/accounts/username/'+search_username
-jss_account_response = requests.get(jss_account_api, auth=(username, password), headers={'Accept': 'application/json'})
+	if jss_users_response.status_code == 200:
+		jss_user = jss_users_response.json()
+		print(jss_user)
 
-if jss_account_response.status_code == 200:
-    jss_account = jss_account_response.json()
-    print(jss_account)
+	# search jamf pro user accounts
+	jss_account_api=api_url+'/accounts/username/'+search_username
+	jss_account_response = requests.get(jss_account_api, auth=(username, password), headers={'Accept': 'application/json'})
 
-# search ldap user accounts
-ldap_servers_url=api_url+'/ldapservers'
-ldap_servers_response = requests.get(ldap_servers_url, auth=(username, password), headers={'Accept': 'application/json'})
+	if jss_account_response.status_code == 200:
+		jss_account = jss_account_response.json()
+		print(jss_account)
 
-if ldap_servers_response.status_code == 200:
-    ldap_servers = ldap_servers_response.json()
-    servers = ldap_servers['ldap_servers']
+	# search ldap user accounts
+	ldap_servers_url=api_url+'/ldapservers'
+	ldap_servers_response = requests.get(ldap_servers_url, auth=(username, password), headers={'Accept': 'application/json'})
 
-    for server in servers:
-        print(server['name'])
-        ldap_server_id = str(server['id'])
-        ldap_server_search_url = api_url+'/ldapservers/id/'+ldap_server_id+'/user/'+search_username
-        ldap_server_search_response = requests.get(ldap_server_search_url, auth=(username, password), headers={'Accept': 'application/json'})
+	if ldap_servers_response.status_code == 200:
+		ldap_servers = ldap_servers_response.json()
+		servers = ldap_servers['ldap_servers']
 
-        if ldap_server_search_response.status_code == 200:
-            ldap_server_search_response_json = ldap_server_search_response.json()
-            print(ldap_server_search_response_json)
+		for server in servers:
+			ldap_server_id = str(server['id'])
+			ldap_server_search_url = api_url+'/ldapservers/id/'+ldap_server_id+'/user/'+search_username
+			ldap_server_search_response = requests.get(ldap_server_search_url, auth=(username, password), headers={'Accept': 'application/json'})
+
+			if ldap_server_search_response.status_code == 200:
+				ldap_server_search_response_json = ldap_server_search_response.json()
+				print(server['name'])
+				print(ldap_server_search_response_json)
+
+	again=input('Search new user?: [y/n] ')
+	if again == 'n':
+		loop = False
