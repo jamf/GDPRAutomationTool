5de1c148967b7922ad7fd878a73563e8e419b46e
Connor Temple <connor.temple@jamf.com>
2018-06-26 14:23:31 -0500

    RES-1267 Split into functions

--- gdpr.py
+++ gdpr.py
@@ -1,64 +1,34 @@
-import sys
 import getpass
 import requests
 import json
 
-instance = input("Jamf Pro URL:")
-username = input("Jamf Pro Username:")
-password = getpass.getpass("Jamf Pro Password:")
 
-api_url = 'https://' + instance + '/JSSResource'
-
-loop = True
-while loop:
-    user_data = {}
-    search_username = input("Search Username:")
-
-    # search jamf pro users
+# search jamf pro users
+def search_user(api_url, api_auth, search_username):
     jss_users_api = api_url + '/users/name/' + search_username
-    jss_users_response = requests.get(jss_users_api, auth=(username, password), headers={'Accept': 'application/json'})
+    jss_users_response = requests.get(jss_users_api, auth=api_auth, headers={'Accept': 'application/json'})
 
     if jss_users_response.status_code == 200:
         jss_user = jss_users_response.json()
-        user_data['user'] = jss_user['user']
         print('User found')
+        return jss_user['user']
 
-        # user's mobile devices
-        mobile_devices = []
-        for device in jss_user['user']['links']['mobile_devices']:
-            device_api_url = api_url + '/mobiledevices/id/' + str(device['id'])
-            device_response = requests.get(device_api_url, auth=(username, password), headers={'Accept': 'application/json'})
-            device_response_json = device_response.json()
-            mobile_devices.append(device_response_json['mobile_device'])
 
-        if len(mobile_devices) > 0:
-            user_data['devices'] = mobile_devices
-            print(str(len(mobile_devices)) + ' mobile devices found')
-
-        # user's computers
-        computers = []
-        for device in jss_user['user']['links']['computers']:
-            device_api_url = api_url + '/computers/id/' + str(device['id'])
-            device_response = requests.get(device_api_url, auth=(username, password), headers={'Accept': 'application/json'})
-            device_response_json = device_response.json()
-            computers.append(device_response_json['computer'])
-
-        if len(computers) > 0:
-            user_data['computers'] = computers
-            print(str(len(computers)) + ' computers found')
-
-    # search jamf pro user accounts
+# search jamf pro accounts
+def search_account(api_url, api_auth, search_username):
     jss_account_api = api_url + '/accounts/username/' + search_username
-    jss_account_response = requests.get(jss_account_api, auth=(username, password), headers={'Accept': 'application/json'})
+    jss_account_response = requests.get(jss_account_api, auth=api_auth, headers={'Accept': 'application/json'})
 
     if jss_account_response.status_code == 200:
         jss_account = jss_account_response.json()
-        user_data['account'] = jss_account['account']
-        print(jss_account)
+        print('Account found')
+        return jss_account['account']
 
-    # search ldap user accounts
+
+# search ldap user accounts
+def search_ldap_account(api_url, api_auth, search_username):
     ldap_servers_url = api_url + '/ldapservers'
-    ldap_servers_response = requests.get(ldap_servers_url, auth=(username, password), headers={'Accept': 'application/json'})
+    ldap_servers_response = requests.get(ldap_servers_url, auth=api_auth, headers={'Accept': 'application/json'})
 
     if ldap_servers_response.status_code == 200:
         ldap_servers = ldap_servers_response.json()
@@ -68,27 +38,98 @@ while loop:
         for server in servers:
             ldap_server_id = str(server['id'])
             ldap_server_search_url = api_url + '/ldapservers/id/' + ldap_server_id + '/user/' + search_username
-            ldap_server_search_response = requests.get(ldap_server_search_url, auth=(username, password), headers={'Accept': 'application/json'})
+            ldap_server_search_response = requests.get(ldap_server_search_url, auth=api_auth, headers={'Accept': 'application/json'})
 
             if ldap_server_search_response.status_code == 200:
                 ldap_server_search_response_json = ldap_server_search_response.json()
                 ldap_results.append({server['name']: ldap_server_search_response_json['ldap_users']})
                 print('LDAP account found on: ' + server['name'])
 
-    if len(ldap_results) > 0:
-        user_data['ldap'] = ldap_results
+        if len(ldap_results) > 0:
+            return ldap_results
 
-    # only write to file if data exists
-    if user_data:
-        file_name = instance + '_' + search_username + '.json'
-        file = open(file_name, 'w')
-        file_contents = json.dumps(user_data)
-        file.write(file_contents)
-        file.close()
-        print('Saved: ' + file_name)
-    else:
-        print('No results found for: ' + search_username)
 
-    again = input('Search new user?: [y/n] ')
-    if again == 'n':
-        loop = False
+# get user's mobile devices
+def get_mobile_devices(api_url, api_auth, devices):
+    mobile_devices = []
+    for device in devices:
+        device_api_url = api_url + '/mobiledevices/id/' + str(device['id'])
+        device_response = requests.get(device_api_url, auth=api_auth, headers={'Accept': 'application/json'})
+        device_response_json = device_response.json()
+        mobile_devices.append(device_response_json['mobile_device'])
+
+    if len(mobile_devices) > 0:
+        print(str(len(mobile_devices)) + ' mobile devices found')
+        return mobile_devices
+
+
+# get user's computers
+def get_computers(api_url, api_auth, devices):
+    computers = []
+    for device in devices:
+        device_api_url = api_url + '/computers/id/' + str(device['id'])
+        device_response = requests.get(device_api_url, auth=api_auth, headers={'Accept': 'application/json'})
+        device_response_json = device_response.json()
+        computers.append(device_response_json['computer'])
+
+    if len(computers) > 0:
+        print(str(len(computers)) + ' computers found')
+        return computers
+
+
+def main():
+    print("GDRP")
+
+    instance = input("Jamf Pro URL:")
+    username = input("Jamf Pro Username:")
+    password = getpass.getpass("Jamf Pro Password:")
+
+    api_url = 'https://' + instance + '/JSSResource'
+    api_auth = (username, password)
+
+    loop = True
+    while loop:
+        user_data = {}
+
+        search_username = input("Search Username:")
+
+        account_result = search_account(api_url, api_auth, search_username)
+        if account_result is not None:
+            user_data['account'] = account_result
+
+        ldap_result = search_ldap_account(api_url, api_auth, search_username)
+        if ldap_result is not None:
+            user_data['ldap'] = ldap_result
+
+        user_result = search_user(api_url, api_auth, search_username)
+        if user_result is not None:
+            user_data['user'] = user_result
+
+            mobile_devices = user_data['user']['links']['mobile_devices']
+            mobile_devices_result = get_mobile_devices(api_url, api_auth, mobile_devices)
+            if mobile_devices_result is not None:
+                user_data['devices'] = mobile_devices_result
+
+            computers = user_data['user']['links']['computers']
+            computers_result = get_computers(api_url, api_auth, computers)
+            if computers_result is not None:
+                user_data['computers'] = computers_result
+
+        # only write to file if data exists
+        if user_data:
+            file_name = instance + '_' + search_username + '.json'
+            file = open(file_name, 'w')
+            file_contents = json.dumps(user_data)
+            file.write(file_contents)
+            file.close()
+            print('Saved: ' + file_name)
+        else:
+            print('No results found for: ' + search_username)
+
+        again = input('Search new user?: [y/n] ')
+        if again == 'n':
+            loop = False
+
+
+if __name__ == '__main__':
+    main()
